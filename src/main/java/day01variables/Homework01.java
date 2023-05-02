b9c793c9dc37157c6ffec65c450884c9193b6603
ARahim-S <arahim.s@yandex.com>
2023-05-02 15:01:41 +0300

    first lesson

---
+++ src/main/java/day01variables/Homework01.java
@@ -0,0 +1,33 @@
+package day01variables;
+
+public class Homework01 {
+    public static void main(String[] args){
+
+        //3 different data types (primitive data types)
+
+        char letter = 'a';
+        int number = 1578954652;
+        double price = 18.99;
+
+        System.out.println(letter);
+        System.out.println(number);
+        System.out.println(price);
+
+        //2 different data (non- primitive data )
+
+        String country = "USA";
+        String state = "Texas";
+
+
+        System.out.println("Country = " + country);
+        System.out.println("State= "+ state);
+
+        //Sum of the 2 different int data types
+
+        int numberA = 180;
+        int numberB = 230;
+
+        System.out.println("Sum = " + (numberA + numberB));
+
+    }
+}
