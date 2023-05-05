886d7fc43ce3ca35e981c1aca4ba2b415c994522
ARahim-S <arahim.s@yandex.com>
2023-05-05 21:39:17 +0300

    updated

--- src/main/java/day03methodobjectcreationscanner/MethodCreation01.java
+++ src/main/java/day03methodobjectcreationscanner/MethodCreation01.java
@@ -11,32 +11,30 @@ public class MethodCreation01 {
         //method olusturmak icin ikinci yol
         //Ornek 2 ikinci yol: Girilen bir kelimeyi ekrana yazdiran bir method olusturup kullaniniz.
         //main method icinde iken method ta kullanilacak olan variable lari olusturunuz
-        String str ="TechproEdu";
+        String str = "TechproEdu";
         //method ismi ve parametreleri yazariz
         //kirmizi kismin uzerinde bekleyip create method a tiklariz intellij otomatik olarak bir method olusturur
         //daha sonra kendi istegimize gore dizayn ederiz.
         printConsole(str);
 
         //Ornek 3: Verilen iki tam sayiyi carpan ve sonucu ekrana yazdiran methodu olusturup kullaniniz.
-        int a=3;
-        int b=5;
-        carpmaYap(a,b);
+        int a = 3;
+        int b = 5;
+        carpmaYap(a, b);
 
         //Ornek 4: Verilen bir tamsayinin karesini hesaplayip console a yazdiran methodu olusturup kullaniniz.
-        int sayi =5;
+        int sayi = 5;
         karesiniAl(sayi);
-
-
 
 
     }
 
     private static void karesiniAl(int sayi) {
-        System.out.println(sayi*sayi);
+        System.out.println(sayi * sayi);
     }
 
     private static void carpmaYap(int a, int b) {
-        System.out.println(a*b);
+        System.out.println(a * b);
     }
 
     public static void printConsole(String str) {
@@ -50,13 +48,17 @@ public class MethodCreation01 {
     //Note: Access modifier i default yapmak isterseniz access modifier i YAZMAYINIZ..
 
     //Ornek 2: Girilen bir kelimeyi ekrana yazdiran bir method olusturup kullaniniz.
-    public static void print(String str){
+    public static void print(String str) {
         System.out.println(str);
     }
     //Eger bir method yeni bir data uretmiyor ise return type i void olur
     //methodun return type i void ise method body icinde return keywordu yazilmaz.
 
 
+    /*
+    1)  Cemberin çevresini hesaplayan methodu oluşturunuz kullanınız
+    2)  Dairenin alanını hesaplayn methodu oluşturunuz ve kullanınız.
+     */
 
 
 }
