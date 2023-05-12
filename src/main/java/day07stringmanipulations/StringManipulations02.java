ba8ed85aa4f0bca7c1cbf6e414b56fc67dec447b
ARahim-S <arahim.s@yandex.com>
2023-05-12 22:38:42 +0300

    updated

--- src/main/java/day07stringmanipulations/StringManipulations02.java
+++ src/main/java/day07stringmanipulations/StringManipulations02.java
@@ -25,6 +25,23 @@ public class StringManipulations02 {
         boolean second = !pwd.contains(" ");
         System.out.println("second = " + second);
 
+        //3) En az bir buyuk harf olsun
+        //Note:Buyuk harf olmayanlari sil
+        //     kalan character sayisina bak
+        //     character sayisi sifir ise buyuk harf yok demektir
+        //      sifirdan buyuk ise buyuk harf var demektir
+        boolean third = pwd.replaceAll("[^A-Z]", "").length() > 0;//Methodlari ayni satirda yanyana kullanmaya method chain denilir
+        System.out.println("third = " + third);
+
+        //4)  4) En az bir kucuk harf olsun
+        boolean fourth = pwd.replaceAll("[^a-z]", "").length() > 0;
+        System.out.println("fourth = " + fourth);
+
+        //  5) En az bir rakam olsun
+        boolean fifth = pwd.replaceAll("[^0-9]", "").length() > 0;
+        System.out.println("fifth = " + fifth);
+
+        System.out.println(("Password gecerli mi?  "+(first && second && third && fourth && fifth)));
 
     }
 }
