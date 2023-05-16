e0146965b88776f6fbcfa804ecaf47330f52c277
ARahim-S <arahim.s@yandex.com>
2023-05-16 13:03:36 +0300

    updated

---
+++ src/main/java/day10ifstatements/NestedIf02.java
@@ -0,0 +1,12 @@
+package day10ifstatements;
+
+public class NestedIf02 {
+    public static void main(String[] args) {
+                /*
+          Example 2:Kullanicidan bir ucgenin kenar uzunluklarini aliniz
+                    Uc kenari birbirine esit ise "Eskenar Ucgen " yazdiriniz
+                    Sadece iki kenar uzunlugu biribirine esit ise "Ikizkenar ucgen"
+                    Tum kenarlari birbirinden farkli ise "Cesitkenar Ucgen" yazdiriniz
+         */
+    }
+}
