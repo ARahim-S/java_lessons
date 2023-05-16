90ec2a36783c5aead7dd8c84cda1822f8142e913
ARahim-S <arahim.s@yandex.com>
2023-05-16 09:37:41 +0300

    updated

--- src/main/java/day09ifstatements/IfStatements03.java
+++ src/main/java/day09ifstatements/IfStatements03.java
@@ -1,15 +1,41 @@
 package day09ifstatements;
 
+
 import java.util.Scanner;
+
 
 public class IfStatements03 {
     public static void main(String[] args) {
-        //Ornek : Kullanicinin vermis oldugu character buyuk harf ise ekrana "Buyuk Harf",
-        // kucuk harf ise ekrana "Kucuk Harf" yazdiran kodu olusturunuz
+
+        //Ornek : Verilen karakter buyuk harf ise ekrana "Buyuk harf" yazdiran
+        // kucuk harf ise "Kucuk Harf" yazdiran kodu olusturunuz
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Lütfen bir karakter giriniz..");
+        System.out.println("Lutfen bir character giriniz..");
         char ch = input.next().charAt(0);
+
+        //1. Yol
+        if (ch >= 'A' && ch <= 'Z') {
+            System.out.println("Buyuk Harf..");
+        }
+        if (ch >= 'a' && ch <= 'z') {
+            System.out.println("Kucuk harf");
+        }
+
+        //2.yol
+
+        if (ch >= 'A' && ch <= 'Z') {
+
+            System.out.println("Buyuk Harf..");
+
+        } else if (ch >= 'a' && ch <= 'z') {
+
+            System.out.println("Kucuk harf");
+
+        } else {
+
+            System.out.println("Harf degil");
+        }
 
 
     }
