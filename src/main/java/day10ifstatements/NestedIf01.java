0cbdab5dd1bde71aff7fd72de504e6f9a103db10
ARahim-S <arahim.s@yandex.com>
2023-05-16 13:02:56 +0300

    updated

--- src/main/java/day10ifstatements/NestedIf01.java
+++ src/main/java/day10ifstatements/NestedIf01.java
@@ -1,5 +1,7 @@
 package day10ifstatements;
 
+import java.util.Scanner;
+
 public class NestedIf01 {
     public static void main(String[] args) {
     /*  Example 1: Kullanicidan 3 tane sayi aliniz.
@@ -12,6 +14,28 @@ public class NestedIf01 {
                  a+c>b>|a-c|
                  b+c>a>|b-c|
                  a=b=c ise eskenar ucgen     */
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter 3 numbers..");
+
+        double a = Math.abs(input.nextDouble());
+        double b = Math.abs(input.nextDouble());
+        double c = Math.abs(input.nextDouble());
+
+        boolean isTriangle = (a + b > c && c > Math.abs(a - b)) &&  // a+b>c>|a-b|)
+                (a + c > b && b > Math.abs(a - c)) &&  // a+c>b>|a-c|
+                (b + c > a && a > Math.abs(b - c));    // b+c>a>|b-c|
+
+        if (isTriangle) {
+
+            if (a == b && b == c && a == c) {
+                System.out.println("Equilateral Triangle ");
+            } else {
+                System.out.println("Triangle but not equilateral ");
+            }
+
+        } else {
+            System.out.println("Not triangle");
+        }
 
 
     }
