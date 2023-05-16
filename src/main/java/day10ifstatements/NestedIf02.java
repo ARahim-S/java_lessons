b3c456188fa6e7905e1c8d6d828c14f802e1d85b
ARahim-S <arahim.s@yandex.com>
2023-05-16 13:16:43 +0300

    updated

--- src/main/java/day10ifstatements/NestedIf02.java
+++ src/main/java/day10ifstatements/NestedIf02.java
@@ -1,5 +1,7 @@
 package day10ifstatements;
 
+import java.util.Scanner;
+
 public class NestedIf02 {
     public static void main(String[] args) {
                 /*
@@ -8,5 +10,36 @@ public class NestedIf02 {
                     Sadece iki kenar uzunlugu biribirine esit ise "Ikizkenar ucgen"
                     Tum kenarlari birbirinden farkli ise "Cesitkenar Ucgen" yazdiriniz
          */
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ucgenin kenarlari icin 3 tane uzunluk giriniz..");
+
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double c = input.nextDouble();
+
+        boolean ucgenmi = (a + b > c && c > Math.abs(a - b)) &&  // a+b>c>|a-b|)
+                (a + c > b && b > Math.abs(a - c)) &&  // a+c>b>|a-c|
+                (b + c > a && a > Math.abs(b - c));    // b+c>a>|b-c|
+
+        if (a <= 0 || b <= 0 || c <= 0) {
+
+            System.out.println("Gecerli kenar uzunlugu giriniz");
+
+        } else if (ucgenmi) {
+
+            if (a == b && b == c && a == c) {
+                System.out.println("Eskenar");
+            } else if (a == b || b == c || a == c) {
+                System.out.println("Ikizkenar");
+            } else {
+                System.out.println("Cesitkenar");
+            }
+
+        } else {
+            System.out.println("Ucgen degildir");
+        }
+
+
     }
 }
