82273b39c4d8eee30b39398db5ab377ca3669952
ARahim-S <arahim.s@yandex.com>
2023-05-16 12:18:32 +0300

    updated

--- src/main/java/day10ifstatements/NestedIf01.java
+++ src/main/java/day10ifstatements/NestedIf01.java
@@ -2,6 +2,17 @@ package day10ifstatements;
 
 public class NestedIf01 {
     public static void main(String[] args) {
+    /*  Example 1: Kullanicidan 3 tane sayi aliniz.
+                 Bu 3 sayinin ucgen olusturma durumunu kontrol ediniz
+                 Eger ucgen ise "eskenar" Ucgen olma durumunu kontrol ediniz.
+                 INFO :
+                 Ucgen Olma Sarti: herhangi iki kenar toplami ucuncu kenarda buyuk olmali
+                                   herhangi iki kenar farki ucuncu kenardan kucuk olmali
+                 a+b>c>|a-b|
+                 a+c>b>|a-c|
+                 b+c>a>|b-c|
+                 a=b=c ise eskenar ucgen     */
+
 
     }
 }
