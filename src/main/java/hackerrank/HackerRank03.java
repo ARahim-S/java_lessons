d3cb0a9fa07c9186339a3ea050065964dddf8fdb
ARahim-S <arahim.s@yandex.com>
2023-05-17 11:16:24 +0300

    updated

---
+++ src/main/java/hackerrank/HackerRank03.java
@@ -0,0 +1,24 @@
+package hackerrank;
+
+import java.util.*;
+import java.io.*;
+
+
+public class HackerRank03 {
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = in.nextInt();//a=0
+            int b = in.nextInt();//b=2
+            int n = in.nextInt();//n=5
+            for (int z=0; z<n; z++) {
+                double z1 = a + Math.pow(2,z)*b; // 0 + 1*2
+
+            }
+
+        }
+
+        in.close();
+    }
+}
