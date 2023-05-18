098f3889bd8c48ebe2a64e1c6926cd862367a0fa
ARahim-S <arahim.s@yandex.com>
2023-05-18 10:15:28 +0300

    updated

--- src/main/java/tasks/Task03Answer.java
+++ src/main/java/tasks/Task03Answer.java
@@ -1,7 +1,20 @@
 package tasks;
 
+import java.util.Scanner;
+
 public class Task03Answer {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lütfen bir ifade giriniz...");
+        String userInput = input.nextLine();
+
+        int isWhiteSpace = userInput.replaceAll("\\S","").length();
+        if(isWhiteSpace>0){
+            System.out.println(isWhiteSpace + " boşluk karakteri girdiniz");
+        }else{
+            System.out.println("boşluksuz");
+        }
 
     }
 }
