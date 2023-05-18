90132c96a2c46dc6a05e57b9fdfd23877b5b3eb2
ARahim-S <arahim.s@yandex.com>
2023-05-18 10:19:24 +0300

    updated

---
+++ src/main/java/tasks/Task04Answer.java
@@ -0,0 +1,16 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class Task04Answer {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lütfen ad soyad giriniz...");
+        String userName = input.nextLine();
+        String firstName = userName.split(" ")[0];
+        String lastName = userName.split(" ")[1];
+        System.out.println("firstName = " + firstName);
+        System.out.println("lastName = " + lastName);
+
+    }
+}
