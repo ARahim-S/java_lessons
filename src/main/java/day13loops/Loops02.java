f83db38688a26f1641c9b49efd7945c1b9e3db20
ARahim-S <arahim.s@yandex.com>
2023-05-23 09:55:36 +0300

    updated

--- src/main/java/day13loops/Loops02.java
+++ src/main/java/day13loops/Loops02.java
@@ -25,6 +25,8 @@ public class Loops02 {
             }
         }
 
+
+
         //Ornek 3: Verilen bir string de ilk a harfinden onceki tum characterleri console a yazdiriniz
         //   "I love Java"  ==> "I love J"
 
@@ -38,6 +40,14 @@ public class Loops02 {
             }
             System.out.print(ch);
         }
+        System.out.println();
+
+        //Başka çözüm "break kullanmadan çözdüm"
+        int aIndex = s1.indexOf("a");
+        for(int i = 0; i< aIndex; i++){
+            System.out.print(s1.charAt(i));
+        }
+
         System.out.println();
 
         //Ornek 4: Verilen bir string te son 'a' dan sonraki tum characterleri ters sirada yazdiriniz
